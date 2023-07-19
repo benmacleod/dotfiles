c9ba863efd9ba5101e0ffacc7ca38a2b882292eb
Ben MacLeod <ben.j.macleod@gmail.com>
2023-07-19 01:29:37 +0000

    Remove lazygit archive after installation

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep '"tag_name":' |  sed -E 's/.*"v*([^"]+)".*/\1/')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 sudo tar xf lazygit.tar.gz -C /usr/local/bin lazygit
+sudo rm lazygit.tar.gz
 
 # Install the keys for the GitHub CLI (gh)
 # https://cli.github.com/manual/installation#linux
